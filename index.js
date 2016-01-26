364164b9bf698a13169e9ebdf575d145df408865
vizzaccaro <phil@netapplications.com>
2016-01-26 13:39:42 -0800

    initial push

--- index.js
+++ index.js
@@ -5,47 +5,56 @@
 
 module.exports = getProfile;
 
+/*
+    Get a profile.
+    Returns:
+    1) An error object if one occurs
+    2) The profile object
+ */
 function getProfile(params, callback) {
 
-    var query = '?ip=' + params.ip, path = '/analytics/profile';
+    try {
 
-    //  get an access token for full access at www.gator.io
-    if (params.accessToken) {
-        query += 'accessToken=' + params.accessToken;
-    } else {
+        var query = '?ip=' + params.ip, path = '/profile';
 
-        //  if no access token, use the free version
-        path += '/free';
-    }
+        //  get an access token for full access at www.gator.io
+        if (params.accessToken) {
+            query += 'accessToken=' + params.accessToken;
+        } else {
 
-    if (params.ua)
-        query += 'ua=' + encodeURIComponent(params.ua);
+            //  if no access token, use the free version
+            path += '/free';
+        }
 
-    //  referrer is used to get search engine, keywords and some quality info
-    if (params.referrer)
-        query += 'referrer=' + encodeURIComponent(params.referrer);
+        if (params.ua)
+            query += 'ua=' + encodeURIComponent(params.ua);
 
-    const https = require('https');
+        //  referrer is used to get search engine, keywords and some quality info
+        if (params.referrer)
+            query += 'referrer=' + encodeURIComponent(params.referrer);
 
-    var options = {
-        hostname: 'api.gator.io',
-        port: 443,
-        path: path + query,
-        method: 'GET'
-    };
+        const https = require('https');
 
-    var req = https.request(options, function(res) {
-        console.log('statusCode: ', res.statusCode);
-        console.log('headers: ', res.headers);
+        var options = {
+            hostname: 'api.gator.io',
+            port: 443,
+            path: path + query,
+            method: 'GET'
+        };
 
-        res.on('data', function(d) {
-            process.stdout.write(d);
+        var req = https.request(options, function(res) {
+
+            res.on('data', function(d) {
+                callback(null, d);
+            });
         });
-    });
-    req.end();
+        req.end();
 
-    req.on('error', function(e) {
-        console.error(e);
-    });
+        req.on('error', function(err) {
+            callback(err);
+        });
+    } catch(err) {
+        callback(err);
+    }
 }
 
